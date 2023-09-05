919616c4d5d16c4c72f87fd286d2b7d709273879
Ivan Johnson <iejohnson@uiowa.edu>
2023-09-05 16:40:24 -0500

    ADD: Initial Commit
    Added basic Bootstraping
    Skeleton for the project

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "scecret-key-goes-here"
+login_manager = LoginManager(app)
+from app import routes
+
+# The login_manager object from the flask_login extension is then created, and its init_app() method is passed the app variable.
---
+++ app/models/user.py
@@ -0,0 +1,43 @@
+from app import login_manager
+
+
+class User:
+    def __init__(self, name, email, password, user_id=None):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f"<User {self.email}>"
+
+    def get_user(self):
+        return self.name, self.email, self.password
+
+    def get_name(self):
+        return self.name
+    def get_id(self):
+        return self.user_id
+
+    def get_email(self):
+        return self.email
+
+    def get_password(self):
+        return self.password
+
+    def set_email(self, email):
+        self.email = email
+
+    def set_password(self, password):
+        self.password = password
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    # Replace this with your actual user loading logic (e.g., database lookup)
+    return User("Test User", "test@test.com", "testpassword", user_id=user_id )
+
+# Path: app/models/post.py
+# Compare this snippet from app/routes.py:
+# # app/routes.py
+#
