4b8a5a51ba2b9412ebc66d6119aa24aa56bb6121
Ivan Johnson <iejohnson@uiowa.edu>
2023-10-02 14:45:30 -0500

    ENH: Got login.html showing up

--- api/views.py
+++ api/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 
 class CustomLoginView(LoginView):
-    template_name = 'greenhouse/login.html'  # Specify the path to your login template
+    template_name = 'login.html'  # Specify the path to your login template
     # You can add more customization options here, such as success and failure URLs, etc.
