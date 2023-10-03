ca2eb8accef990a1deb55a6943ccb4e2c12c2b9e
Ivan Johnson <iejohnson@uiowa.edu>
2023-10-02 20:49:44 -0500

    WIP: Attempting to Init db

--- main/models.py
+++ main/models.py
@@ -2,6 +2,9 @@
 
 from django.db import models
 
+import backend.settings
+
+
 class SensorReading(models.Model):
     temperature = models.FloatField()
     humidity = models.FloatField()
@@ -28,3 +31,33 @@ class ControlSignals(models.Model):
 
     def __str__(self):
         return f"Heating Pad: {self.heating_pad}, Light: {self.light}"
+
+
+if __name__ == "__main__":
+    from .models import Greenhouse, ControlSignals
+    from django.contrib.auth.models import User  # Import the User model
+    from backend import settings  # Import the DATABASES dictionary
+    import os
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", DEFAULT_SETTINGS_MODULE)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myapp.settings")
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", INSTALLED_APPS)  # Set the DJANGO_SETTINGS_MODULE environment variable
+    # Create a user if not already created
+    user, created = User.objects.get_or_create(username="ivanjohnson-test")
+    settings.configure()  # Configure the database settings
+    # Create a greenhouse instance with initial values
+    greenhouse = Greenhouse(
+        name="Your Greenhouse Name",
+        description="Your Greenhouse Description",
+        location="Your Location",
+        user=user,  # Assign the user you created
+    )
+    greenhouse.save()  # Save the greenhouse instance
+
+    # Create a control signal instance associated with the greenhouse
+    control_signals = ControlSignals(
+        heating_pad=False,
+        light=False,
+        greenhouse=greenhouse,  # Associate with the created greenhouse
+    )
+    control_signals.save()  # Save the control signals instance
